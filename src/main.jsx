0b7e10f9c85868e02edf19cb6e9407cf0d6f84c9
ChronoKross <nathanblainegwyn@gmail.com>
2023-10-09 22:39:36 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       </Routes>
     </BrowserRouter>
 
-    {location.path !== "/dashboard" && <Stack />}
+    {/* {location.path !== "/dashboard" && <Stack />} */}
   </React.StrictMode>
 );
