1da7da599ce23badff37a7731c86745d543fd847
ChronoKross <nathanblainegwyn@gmail.com>
2023-10-09 22:31:07 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,23 +6,18 @@ import Dashboard from "./pages/Dashboard.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login.jsx";
-import { UserContext } from "./context.js/context.js";
-
-let admin = localStorage.getItem("admin");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <UserContext.Provider value={user}> */}
       <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/" element={<Stack />} />
       </Routes>
-      {/* </UserContext.Provider> */}
     </BrowserRouter>
 
-    {/* {location.path !== "/dashboard" && <Stack />} */}
+    {location.path !== "/dashboard" && <Stack />}
   </React.StrictMode>
 );
