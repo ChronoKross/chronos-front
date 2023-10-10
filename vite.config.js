0ad623a1d46e66f4d47ea111e2f26131e120c219
ChronoKross <nathanblainegwyn@gmail.com>
2023-10-09 23:28:33 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./", // Set the base path to support hash-based routing
-  esbuild: {
-    jsxFactory: "React.createElement", // Configure the jsx pragma
-    jsxInject: "import React from 'react'", // Import React for JSX
-  },
 });
