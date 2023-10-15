7cedd3909299c5300c9b132e714c576422246d6c
ChronoKross <nathanblainegwyn@gmail.com>
2023-10-15 00:51:07 -0400

    updated DASHBOARD
    test

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,36 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 export default function Dashboard() {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://repreveback-end.onrender.com/api/employees") // Replace with the actual API endpoint
+      .then((res) => {
+        setEmployees(res.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching employees:", error);
+      });
+  }, []);
+
   return (
-    <div
-      className=" h-max w-max flex justify-center items-center text-4xl font-bold text-white
-     bg-gray-950  "
-    >
-      <div className="">Dashboard</div>
+    <div className="h-screen w-screen flex justify-center items-center text-4xl font-bold text-white bg-gray-950">
+      <div>
+        {employees.map((employee) => (
+          <div key={employee._id}>
+            {employee.timeOff.map((timeOff) => {
+              <ul>
+                <li>{timeOff}</li>
+              </ul>;
+            })}
+            <p>{employee.name}</p>
+            <p>{employee.position}</p>
+            {/* Display other employee details as needed */}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
